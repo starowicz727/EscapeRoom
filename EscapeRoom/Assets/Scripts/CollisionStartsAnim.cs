a993da0dab70ad1b262b7f5edbb73b30c16f9017
starowicz727 <starowicz727@gmail.com>
2021-12-13 10:45:37 +0100

    skrypt kolizje z odtwarzaczem vhs

--- EscapeRoom/Assets/Scripts/CollisionStartsAnim.cs
+++ EscapeRoom/Assets/Scripts/CollisionStartsAnim.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollisionStartsAnim : MonoBehaviour
-{
+public class CollisionStartsAnim : MonoBehaviour  //skrypt przypisany do przedmiotu który nie znika ze sceny 
+{                                                 //czyli np. odtwarzacz VHS, szafa
     public GameObject collisionParticipant;
     public ParticleSystem explosion;
-    public Animation resultingAnimation;
+    //public Animation resultingAnimation;
     private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.Equals(collisionParticipant))
         {
             explosion.gameObject.SetActive(true);
-            resultingAnimation.Play();
+            //resultingAnimation.Play();
             collisionParticipant.SetActive(false);
         }
     }
