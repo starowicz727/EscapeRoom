19d91ee5a244d859518c1e3678264a5823b18903
DamMrowiec <mrowiec.damian@wp.pl>
2021-12-13 16:56:35 +0100

    zamek/portal scena 2

--- EscapeRoom/Assets/Scripts/CzyOtwarte.cs
+++ EscapeRoom/Assets/Scripts/CzyOtwarte.cs
@@ -6,35 +6,68 @@ public class CzyOtwarte : MonoBehaviour
 {
     private int[] result, correctCombination;
     public GameObject portal;
+    public int Scena;
     // Start is called before the first frame update
     private void Start()
     {
-        result = new int[] { 5, 5, 5, 5 };
-        correctCombination = new int[] { 2, 4, 1, 0 };
+        if (Scena == 1)
+        {
+            result = new int[] { 5, 5, 5, 5 };
+            correctCombination = new int[] { 2, 4, 1, 0 };
+        }
+        else if (Scena == 2)
+        {
+            result = new int[] { 5, 5, 5};
+            correctCombination = new int[] { 8, 6, 8};
+        }
         Rotacja.Rotated += CheckResults;
     }
     private void CheckResults(string wheelName, int number)
     {
-        switch (wheelName)
+        if (Scena == 1)
         {
-            case "Wheel1":
-                result[0] = number;
-                break;
-            case "Wheel2":
-                result[1] = number;
-                break;
-            case "Wheel3":
-                result[2] = number;
-                break;
-            case "Wheel4":
-                result[3] = number;
-                break;
+            switch (wheelName)
+            {
+                case "Wheel1":
+                    result[0] = number;
+                    break;
+                case "Wheel2":
+                    result[1] = number;
+                    break;
+                case "Wheel3":
+                    result[2] = number;
+                    break;
+                case "Wheel4":
+                    result[3] = number;
+                    break;
+            }
+            if (result[0] == correctCombination[0] && result[1] == correctCombination[1] && result[2] == correctCombination[2] && result[3] == correctCombination[3])
+            {
+                Debug.Log("Opened!");
+                portal.SetActive(true);
+                FindObjectOfType<AudioManager>().Play("portal1");
+            }
         }
-        if (result[0] == correctCombination[0] && result[1] == correctCombination[1] && result[2] == correctCombination[2] && result[3] == correctCombination[3])
+        if (Scena == 2)
         {
-            Debug.Log("Opened!");
-            portal.SetActive(true);
-            FindObjectOfType<AudioManager>().Play("portal1");
+            switch (wheelName)
+            {
+                case "Wheel1":
+                    result[0] = number;
+                    break;
+                case "Wheel2":
+                    result[1] = number;
+                    break;
+                case "Wheel3":
+                    result[2] = number;
+                    break;
+            }
+            if (result[0] == correctCombination[0] && result[1] == correctCombination[1] && result[2] == correctCombination[2])
+            {
+                Debug.Log("Opened!");
+                portal.SetActive(true);
+                FindObjectOfType<AudioManager>().Play("portal1");
+            }
         }
     }
 
