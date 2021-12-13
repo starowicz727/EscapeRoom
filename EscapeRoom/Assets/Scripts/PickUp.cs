cd52c9adf619281a37653b7fd7652aa7d7afe7d4
starowicz727 <starowicz727@gmail.com>
2021-12-13 17:05:08 +0100

    zmiana skryptu podnoszenia

--- EscapeRoom/Assets/Scripts/PickUp.cs
+++ EscapeRoom/Assets/Scripts/PickUp.cs
@@ -12,7 +12,7 @@ public class PickUp : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.Mouse0)) //KeyCode.E
         {
             if (heldObj == null)
             {
