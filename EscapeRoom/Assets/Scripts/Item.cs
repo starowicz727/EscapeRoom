dce89ebe4a7ad72d18d05e7aa929005a497e4ef5
Sonia <67146981+Lara500@users.noreply.github.com>
2021-12-17 13:03:57 +0100

    Usuwanie ubrań

--- EscapeRoom/Assets/Scripts/Item.cs
+++ EscapeRoom/Assets/Scripts/Item.cs
@@ -9,23 +9,4 @@ public class Item : MonoBehaviour
     public string description;
     public Sprite icon;
     public bool pickedUp;
-    [HideInInspector]
-    public bool equipped;
-
-    public void Update()
-    {
-        if(equipped)
-        {
-
-        }
-    }
-
-    public void ItemUsage()
-    {
-        if(type == "Ball")
-        {
-            equipped = true;
-        }
-    }
-
 }
