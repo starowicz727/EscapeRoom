83caf3330d35a689429b55a204a15141cb61a811
PatrycjaPiatek <pi.patrycja99@gmail.com>
2022-01-07 12:29:46 +0100

    lvl labirynt 3

---
+++ EscapeRoom/Assets/LabiryntLvl/Scripts/DaneGry.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DaneGry
+{
+    public static int zycie;
+    public static int destinations;
+    public static int score;
+}
---
+++ EscapeRoom/Assets/LabiryntLvl/Scripts/NavGame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NavGame : MonoBehaviour
+{
+    void Update()
+    {
+        //wyjscie
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        //kolejne podejscie
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Projekt();           
+        }
+        //start
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Projekt();           
+        }      
+    }
+    public void Projekt()
+    {
+        SceneManager.LoadScene("Projekt");
+        DaneGry.score = 100;
+        DaneGry.zycie = 100;
+        DaneGry.destinations = 0;
+    }
+}
