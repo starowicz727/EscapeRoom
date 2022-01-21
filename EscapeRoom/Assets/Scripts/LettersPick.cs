43d3706424c58b76769898dfbc6232a1ef50b2da
Sonia <67146981+Lara500@users.noreply.github.com>
2022-01-21 13:06:55 +0100

    zliczanie literek

--- EscapeRoom/Assets/Scripts/LettersPick.cs
+++ EscapeRoom/Assets/Scripts/LettersPick.cs
@@ -15,6 +15,8 @@ public class LettersPick : MonoBehaviour
     public Transform holeK2;
     public Transform holeU2;
     public Transform holeS2;
+    public static int ileZebranych = 0;
+    public GameObject portal;
 
     private void Start()
     {
@@ -58,42 +60,58 @@ public class LettersPick : MonoBehaviour
         {
             Debug.Log(holeH.GetComponent<RawImage>());
             holeH.GetComponent<RawImage>().enabled = true;
+            ileZebranych++;
         }
         else if (letter.name.Equals("o"))
         {
             holeO.GetComponent<RawImage>().enabled = true;
+            ileZebranych++;
         }
         else if (letter.name.Equals("k"))
         {
             holeK.GetComponent<RawImage>().enabled = true;
+            ileZebranych++;
         }
         else if (letter.name.Equals("u"))
         {
             holeU.GetComponent<RawImage>().enabled = true;
+            ileZebranych++;
         }
         else if (letter.name.Equals("s"))
         {
             holeS.GetComponent<RawImage>().enabled = true;
+            ileZebranych++;
         }
         else if (letter.name.Equals("p"))
         {
             holeP.GetComponent<RawImage>().enabled = true;
+            ileZebranych++;
         }
         else if (letter.name.Equals("o2"))
         {
             holeO2.GetComponent<RawImage>().enabled = true;
+            ileZebranych++;
         }
         else if (letter.name.Equals("k2"))
         {
             holeK2.GetComponent<RawImage>().enabled = true;
+            ileZebranych++;
         }
         else if (letter.name.Equals("u2"))
         {
             holeU2.GetComponent<RawImage>().enabled = true;
+            ileZebranych++;
         }
         else if (letter.name.Equals("s2"))
         {
             holeS2.GetComponent<RawImage>().enabled = true;
+            ileZebranych++;
+        }
+
+
+        if(ileZebranych == 10)
+        {
+            portal.SetActive(true);
         }
     }
 }
